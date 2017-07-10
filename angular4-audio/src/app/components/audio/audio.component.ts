52a001fc1ab401a310541ce24f1e13629c7eff95
Ximelly <ximelly@163.com>
2017-07-10 23:38:08 +0800

    加入音频状态
    加入音频状态

--- angular4-audio/src/app/components/audio/audio.component.ts
+++ angular4-audio/src/app/components/audio/audio.component.ts
@@ -1,45 +1,56 @@
-import { Component, OnInit,ViewChild,ElementRef,EventEmitter, Input, Output } from '@angular/core';
+import { Component,OnInit,ViewChild,ElementRef,EventEmitter,Input,Output,DoCheck} from '@angular/core';
 
 @Component({
   selector: 'app-audio',
-  template: `<audio [src]="audiosrc" controls preload="none" #audioElement></audio>
-            <div><span *ngIf="allTime">总时间：{{allTime}}</span>
-            <span *ngIf="currentTime">当前播放时间：{{currentTime}}'</span>
-            <span *ngIf="progross">播放进度：{{progross}}</span></div>
-            <div>
-              <button (click)="playAudio()">播放</button>
-              <button (click)="pauseAudio()">暂停</button>
-            </div>
-            <br/>
-            `
+  templateUrl: "./audio.component.html",
+  styleUrls:['./audio.component.css']
 })
-export class AudioComponent implements OnInit {
+export class AudioComponent implements OnInit,DoCheck {
   @Input() public audiosrc: string = '';
   @ViewChild('audioElement') _audio: ElementRef;
-  private allTime:number;
-  private currentTime:number;
-  private progross:string;
+  private allTime:number=0;//当前音频总时长
+  public preload:string="none";//是否预加载
+  public loop:boolean=false;//是否循环播放
+  private currentTime:number=0;//当前播放时间
+  private progross:string;//当前播放进度百分比
+  private audo_loading:boolean=false;
+  private audo_error:boolean=false;
+  private audo_status:boolean=false;
   //对外开放的事件
   @Output() public onPlayAudio: EventEmitter<AudioComponent> = new EventEmitter<AudioComponent>();
   @Output() public onPauseAudio: EventEmitter<AudioComponent> = new EventEmitter<AudioComponent>();
-  constructor() { }
-
+  constructor(){ }
   ngOnInit() {
   }
+  ngDoCheck(): void{}
 
   public playAudio(): void{
     this._audio.nativeElement.play();
+    this.audo_status=true;
     setTimeout(()=>{
       this.allTime=parseInt(this._audio.nativeElement.duration);
     },500);
     setInterval(()=>{
-      this.currentTime=parseInt(this._audio.nativeElement.currentTime);
-      this.progross=(-1!=(JSON.stringify(this.currentTime/this.allTime).indexOf(".")))?(JSON.stringify(Math.ceil(this.currentTime*100/this.allTime)) + "%"):"0%";      
+        this.currentTime=parseInt(this._audio.nativeElement.currentTime);
+        this.progross=(-1!=(JSON.stringify(this.currentTime/this.allTime).indexOf(".")))?(JSON.stringify(Math.ceil(this.currentTime*100/this.allTime)) + "%"):"0%";
     },500)
   }
   public pauseAudio(): void{
     this.onPauseAudio.emit(this);
     this._audio.nativeElement.pause();
+    this.audo_status=false;
+  }
+  
+  public handleButton(): void{
+    console.log(this._audio)
+    if(!this._audio.nativeElement.preload){
+      return;
+    }
+    if(this._audio.nativeElement.paused){
+      this.playAudio();
+    }else{
+      this.pauseAudio();
+    }
   }
 
 }
